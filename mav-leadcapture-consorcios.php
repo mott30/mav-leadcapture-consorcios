3c537e9d631876c1cdb175c977ec96456ad72afb
Luciano Tonet <tonetlds@gmail.com>
2014-08-08 08:19:05 -0300

    Inserido as configs do Github

--- mav-leadcapture-consorcios.php
+++ mav-leadcapture-consorcios.php
@@ -64,11 +64,11 @@ add_action( 'wp_enqueue_scripts', 'simulador_bootstrap' );
 if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
     $config = array(
         'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
-        'proper_folder_name' => 'plugin-name', // this is the name of the folder your plugin lives in
-        'api_url' => 'https://api.github.com/repos/username/repository-name', // the github API url of your github repo
-        'raw_url' => 'https://raw.github.com/username/repository-name/master', // the github raw url of your github repo
-        'github_url' => 'https://github.com/username/repository-name', // the github url of your github repo
-        'zip_url' => 'https://github.com/username/repository-name/zipball/master', // the zip url of the github repo
+        'proper_folder_name' => 'mav-leadcapture-consorcios', // this is the name of the folder your plugin lives in
+        'api_url' => 'https://api.github.com/repos/MAVResultadosOnline/mav-leadcapture-consorcios', // the github API url of your github repo
+        'raw_url' => 'https://raw.github.com/MAVResultadosOnline/mav-leadcapture-consorcios', // the github raw url of your github repo
+        'github_url' => 'https://github.com/MAVResultadosOnline/mav-leadcapture-consorcios', // the github url of your github repo
+        'zip_url' => 'https://github.com/MAVResultadosOnline/mav-leadcapture-consorcios/archive/master', // the zip url of the github repo
         'sslverify' => true // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
         'requires' => '3.0', // which version of WordPress does your plugin require?
         'tested' => '3.3', // which version of WordPress is your plugin tested up to?
